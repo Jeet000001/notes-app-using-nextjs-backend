34021ed00a93eff16fdd94c8eff0b59fabb5c30e
Jeet Paul <jeetpaul2222@gmail.com>
2026-03-27 18:05:47 +0530

    some fixes on page component

--- app/page.js
+++ app/page.js
@@ -3,24 +3,25 @@ import React from "react";
 import NoteForm from "./components/Notes";
 import Notes from "@/models/notes";
 
+export const dynamic = "force-dynamic";
+
 const getNotes = async () => {
   await dbconnect();
   const notes = await Notes.find({}).sort({ createdAt: -1 }).lean();
 
   return notes.map((note) => ({
     ...note,
+    _id: undefined,
     id: note._id.toString(),
   }));
 };
 
 const page = async () => {
-  await dbconnect();
   const notes = await getNotes();
-  console.log(notes);
 
   return (
     <div className="min-h-screen">
-        <NoteForm savedNotes={notes} />
+      <NoteForm savedNotes={notes} />
     </div>
   );
 };
